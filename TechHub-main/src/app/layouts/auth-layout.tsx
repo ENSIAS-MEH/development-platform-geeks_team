c62554b95dd5e55b07efcef8aa8b00dde0a69ff2
Alae <182300405+Alae-Labhal@users.noreply.github.com>
2026-03-29 00:55:32 +0100

    adding modifications

--- TechHub-main/src/app/layouts/auth-layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router";
-
-export function AuthLayout() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-[#F0F4F8]">
-      <Outlet />
-    </div>
-  );
-}
